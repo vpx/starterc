499ddaf7ef459dccb25b58b1e45f09cae1493d41
vpx <emanuele.milani06@gmail.com>
2025-08-15 11:50:37 +0000

    Add mylib implementation example

---
+++ include/mylib.h
@@ -0,0 +1,6 @@
+#ifndef STARTERC_MYLIB_H
+#define STARTERC_MYLIB_H
+
+int mylib_sum(int a, int b);
+
+#endif // STARTERC_MYLIB_H
---
+++ src/mylib.c
@@ -0,0 +1,5 @@
+#include "mylib.h"
+
+int mylib_sum(int a, int b) {
+    return a + b;
+}
